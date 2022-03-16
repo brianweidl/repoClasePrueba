77cb6db9a3d20daf82936b111c97caf7ff3fe600
alejandrodh <alejandro@digitalhouse.com>
2022-03-16 13:27:01 -0300

    clase 3 - intro a componentes.

---
+++ clase03_componentes/dashboard/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Sidebar from "./components/Sidebar/Sidebar"
+
+function App() {
+  return (
+   <div id="wrapper">
+      <Sidebar />
+
+      {/* <!-- Content Wrapper --> */}
+		<div id="content-wrapper" class="d-flex flex-column">
+
+			{/* <!-- Main Content --> */}
+			<div id="content">
+
+      </div>
+    </div>
+
+   </div>
+  );
+}
+
+export default App;
