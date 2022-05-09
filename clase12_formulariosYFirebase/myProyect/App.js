5d69c851db84c9946c60d150f9bb0d5727cb3a65
alejandrodh <alejandro@digitalhouse.com>
2022-05-09 20:47:31 -0300

    formularios y Firebase

---
+++ clase12_formulariosYFirebase/myProyect/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity } from 'react-native';
+import Register from './src/screens/Register';
+
+export default function App() {
+
+  return (
+    <View>
+        <Register />
+    </View>
+  );
+}
